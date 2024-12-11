a61381acd97891de81f1c8661c58f140d8a63b01
SaiGowtham26 <x23337818@ncirl.ie>
2024-12-11 22:32:12 +0000

    pylint score update

--- elibrary/settings.py
+++ elibrary/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-from django.core.management.utils import get_random_secret_key
+
 
 
 # Quick-start development settings - unsuitable for production
